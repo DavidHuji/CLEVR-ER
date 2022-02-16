dffc58707da267ce90bd8d7ba7b2cc0971c286d6
DavidHuji <nukraidavid@mail.tau.ac.il>
2022-02-16 21:48:07 +0200

    first benchmark version

---
+++ benchmark/data_handler.py
@@ -0,0 +1,147 @@
+import torch, json, math, matplotlib
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+from os import listdir, system
+from os.path import isfile, join
+import numpy as np
+from PIL import Image
+import random
+
+DATA_PATH = 'liquid_data'
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if str(device) != "cpu":
+    matplotlib.use('pdf')
+
+
+shapes = ["cube", "sphere", "cylinder"]
+colors = ["gray", "blue", "brown", "yellow", "red", "green", "purple", "cyan"]
+shapes_and_colors = [c + ' ' + s for s in shapes for c in colors]
+objects_code = {k: i for i, k in enumerate(shapes_and_colors)}
+loc_rel = {i: k for i, k in enumerate(['left', 'above', 'right', 'below'])}
+
+
+# preprocessing
+import torchvision
+print(torchvision.__version__)
+# from torchvision.transforms.autoaugment import RandAugment
+from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
+
+
+BICUBIC = Image.BICUBIC
+def _convert_image_to_rgb(image):
+    return image.convert("RGB")
+preprocessing = Compose([
+        Resize(224, interpolation=BICUBIC),
+        CenterCrop(224),
+        _convert_image_to_rgb,
+        # RandAugment(),
+        ToTensor(),
+        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+    ])
+
+
+class CustomCLEVRImageDataset(Dataset):
+    def __init__(self, size, data_path=DATA_PATH, use_random_location_of_main_index=False):
+        self.data_path = data_path
+        self.json_paths = [f for f in listdir(join(self.data_path, "label")) if
+                           isfile(join(self.data_path, "label", f))][: size]
+        self.shuffle_indexes = np.random.permutation(len(self.json_paths))
+        self.size = size
+        if size > len(self.json_paths):
+            print(f'There is no enough data for dataset in size {size}')
+
+        self.use_random_location_of_main_index = use_random_location_of_main_index
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        json_name = self.json_paths[self.shuffle_indexes[idx]]
+        label_path = join(self.data_path, "label", json_name)
+
+        with open(label_path, 'r') as j:
+            label = json.loads(j.read())
+        img_path = join(self.data_path, "images", label['image_filename'])
+
+        # img = np.fromfile(img_path, dtype=np.int).reshape(512, 512, 3)
+        img = Image.open(img_path)
+
+        # img = np.array(img)[:, :, :3]
+        img = preprocessing(img)  #.permute(1, 2, 0)
+        # img = torch.tensor(img).float()
+
+        item = self.prepare_item(label)
+        # plt.imshow(img)
+        # plt.show()
+        # self.show_coords(label, Image.open(img_path))
+        # print(label['objects'][item[1][2]]['shape'], label['objects'][item[1][2]]['color'], item[0][-1], label['objects'][(item[1][2] + 1) % 2]['shape'], label['objects'][(item[1][2] + 1) % 2]['color'])
+        indexes = torch.cat(item[1])
+        x, y = (img, indexes), item[0]
+        return x, y
+
+    def prepare_item(self, item):
+        # make one vector for model output + order of objects for reffering expression + original label
+        # the gt y is of size 5 when each entry relates to a different relation
+        if self.use_random_location_of_main_index:
+            random_obg = random.randint(0, 1)  # randomly choose who is the obj and subj from the two possible object
+        else:
+            random_obg = 0
+        obj_1 = item['objects'][random_obg]
+        obj_2 = item['objects'][(random_obg + 1) % 2]
+        name_1 = item['objects'][random_obg]['color'] + ' ' + item['objects'][random_obg]['shape']
+        name_2 = item['objects'][(random_obg + 1) % 2]['color'] + ' ' + item['objects'][(random_obg + 1) % 2]['shape']
+        ndx_1 = torch.nn.functional.one_hot(torch.tensor(objects_code[name_1]), num_classes=len(objects_code))
+        ndx_2 = torch.nn.functional.one_hot(torch.tensor(objects_code[name_2]), num_classes=len(objects_code))
+
+        greater_than = 2 if (obj_1['size'] == 'large' and obj_2['size'] == 'small') else (1 if (obj_2['size'] == 'large' and obj_1['size'] == 'small') else 0) # 2 if a is bigger, 1 if the opsite else (equal) 0
+
+        material_relation = int(obj_1['material'] != 'rubber' and obj_2['material'] == 'rubber')  # binary -a is more sparkly than b
+
+        relative_location = self.calc_location_relation(obj_1['pixel_coords'], obj_2['pixel_coords'])  # 0-3 by angle
+
+        higher_than = int(obj_1['3d_coords'][2] > obj_2['3d_coords'][2])  # binary
+
+        closer_than = int(obj_1['pixel_coords'][2] < obj_2['pixel_coords'][2])  # binary
+
+        flow_relation = 0  # 0 no flow, 1 flow between a to b, 2 the upside direction, 3 and 4 for viscosity
+        if 'liquid_params' in item:
+            if item['liquid_params']['viscosity'] == 0:  # water
+                flow_relation = 1 if obj_1['liquid_src'] else 2
+            elif item['liquid_params']['viscosity'] == 0.05:  # high viscosity
+                flow_relation = 3 if obj_1['liquid_src'] else 4
+            else:
+                print('error - wired viscosity - ', item['liquid_params']['viscosity'])
+
+        return np.array([greater_than, higher_than, material_relation, relative_location, flow_relation, closer_than]), (ndx_1, ndx_2, torch.Tensor([random_obg]))
+        #       amounts of options:  c  #18 options
+
+    @staticmethod
+    def show_coords(label, image):
+        obj_coords = [obj['pixel_coords'] for obj in label['objects']]
+        im = np.array(image)[:, :, :3]
+        for coords in obj_coords:
+            x, y = coords[0], coords[1]
+            im[y:y + 10, x:x + 10, :] = 200
+        plt.imshow(im)
+        plt.show()
+
+    @staticmethod
+    def calc_location_relation(coords_a, coords_b):
+        x_a, y_a, x_b, y_b = coords_a[0], coords_a[1], coords_b[0], coords_b[1]
+        d = x_a - x_b, y_a - y_b
+        theta = math.atan2(d[1], d[0])
+        if theta >= 3 * math.pi / 4 or theta <= -3 * math.pi / 4:
+            return 0  # left
+        elif -3 * math.pi / 4 <= theta < -math.pi / 4:
+            return 1  # above
+        elif -math.pi / 4 <= theta < math.pi / 4:
+            return 2  # right
+        elif math.pi / 4 <= theta < 3 * math.pi / 4:
+            return 3  # 'below'
+
+
+if __name__ == '__main__':
+    a = CustomCLEVRImageDataset(size=10)
+    for i in range(10):
+        b = a.__getitem__(i)
---
+++ benchmark/model.py
@@ -0,0 +1,220 @@
+import math
+
+import numpy as np
+import torch
+from torch import nn
+from torchvision import models
+import os
+import torch, clip
+from torch import nn
+import torch.nn.functional as F
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+import pytorch_lightning as pl
+from data_handler import CustomCLEVRImageDataset
+from torchmetrics import Accuracy
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("using device ", device)
+
+DBG_MODE = not torch.cuda.is_available()
+# print(f'dbg mod ={DBG_MODE}')
+
+DATA_SIZE = 40 if DBG_MODE else 5000
+NUM_WORKERS = 1 if DBG_MODE else 1
+GPUS = 0 if DBG_MODE else 1
+
+amount_of_options_per_relation = [3, 2, 2, 4, 2, 5]
+
+BATCH_SIZE = 2 if DBG_MODE else 8
+mlp_width = int(math.pow(2, 10 if not DBG_MODE else 10))
+amount_of_relations_types, amount_of_options_for_relations_type = 6, 5
+visual_features_size = 128
+
+OUTPUT_MODE = 1  # 0-5 IS FOR SINGLE RELATION, 6 IS FOR ALL REL TOGETHER.
+all_rels_together = OUTPUT_MODE == 6
+relation_to_train = OUTPUT_MODE
+
+if all_rels_together:
+    train_total_rels, train_total_options = amount_of_relations_types, amount_of_options_for_relations_type
+else:
+    train_total_rels, train_total_options = 1, amount_of_options_per_relation[relation_to_train]
+gt_relations_amount = train_total_rels * train_total_options
+
+#model, preprocess = clip.load("ViT-B/32", device=device)
+# # model, preprocess = clip.load("RN50", device=device)
+# model.eval()
+
+losses = [nn.CrossEntropyLoss().to(device) for i in range(amount_of_relations_types)]
+accuracies = [0 for _ in range(amount_of_relations_types)]
+
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, out_size, freeze=True):
+        super(FeatureExtractor, self).__init__()
+        inner_model = models.vgg16(pretrained=True).to(device)
+        # Extract VGG-16 Feature Layers
+        self.features = list(inner_model.features)
+        self.features = nn.Sequential(*self.features)
+        # Extract VGG-16 Average Pooling Layer
+        self.pooling = inner_model.avgpool
+        # Convert the image into one-dimensional vector
+        self.flatten = nn.Flatten()
+        # Extract the first part of fully-connected layer from VGG16
+        self.fc = nn.Linear(25088, out_size)
+
+        for param in self.features.parameters():
+            param.requires_grad = not freeze
+        self.to(device)
+
+    def forward(self, x):
+        self.to(device)
+        # It will take the input 'x' until it returns the feature vector called 'out'
+        out = self.features(x)
+        out = self.pooling(out)
+        out = self.flatten(out)
+        out = self.fc(out)
+        return out
+
+
+class MLP_on_features(pl.LightningModule):
+    def __init__(self, input_dim=(visual_features_size + (2 * 24) + 1), freeze_feat_extract=True):
+        super().__init__()
+        self.mlp_layers = nn.Sequential(
+            nn.Linear(input_dim, mlp_width),
+            nn.ReLU(),
+            nn.Linear(mlp_width, mlp_width),
+            nn.ReLU(),
+            nn.Linear(mlp_width, mlp_width),
+            nn.ReLU(),
+            nn.Linear(mlp_width, mlp_width),
+            nn.ReLU(),
+            nn.Linear(mlp_width, mlp_width),
+            nn.ReLU(),
+            nn.Linear(mlp_width, mlp_width),
+            nn.ReLU(),
+            nn.Linear(mlp_width, gt_relations_amount)
+        ).to(device)
+        self.feat_extract = FeatureExtractor(out_size=visual_features_size, freeze=freeze_feat_extract).to(device)
+        self.feat_extract.to(device)
+        self.to(device)
+
+    def forward(self, x):
+        img, ndxs = x
+        feats = self.feat_extract(img.to(device))
+        mlp_in = torch.cat((feats, ndxs.to(device)), 1).to(device)
+        self.to(device)
+        mlp_out = self.mlp_layers(mlp_in)
+        return torch.reshape(mlp_out, (BATCH_SIZE, train_total_rels, train_total_options))
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y = y.to(torch.float32).to(device)
+        y_hat = self(x)
+        total_loss, accuracy = 0.0, 0.0
+        if all_rels_together:
+            for i in range(train_total_rels):
+                total_loss += losses[i](y_hat[:, i, :], y[:, i].long())
+                accuracy += ((np.argmax(y_hat[:, i, :].cpu().detach().numpy()) == y[:, i].long()) * 1).sum() / BATCH_SIZE
+            accuracy /= amount_of_relations_types
+        else:
+            total_loss += losses[0](y_hat[:, 0, :], y[:, relation_to_train].long())
+            accuracy += ((np.argmax(y_hat[:, 0, :].cpu().detach().numpy()) == y[:, relation_to_train].long()) * 1).sum() / BATCH_SIZE
+
+        self.log("train_loss", total_loss, prog_bar=True)
+        self.log("train_acc", accuracy, prog_bar=True)
+        return total_loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y = y.to(torch.float32).to(device)
+        y_hat = self(x)
+
+        total_loss, accuracy = 0.0, 0.0
+        if all_rels_together:
+            for i in range(train_total_rels):
+                total_loss += losses[i](y_hat[:, i, :], y[:, i].long())
+                accuracy += ((np.argmax(y_hat[:, i, :].cpu().detach().numpy()) == y[:, i].long()) * 1).sum() / BATCH_SIZE
+            accuracy /= amount_of_relations_types
+        else:
+            total_loss += losses[0](y_hat[:, 0, :], y[:, relation_to_train].long())
+            accuracy += ((np.argmax(y_hat[:, 0, :].cpu().detach().numpy()) == y[:, relation_to_train].long()) * 1).sum() / BATCH_SIZE
+
+        self.log("val_loss", total_loss, prog_bar=True)
+        self.log("val_acc", accuracy, prog_bar=True)
+        return total_loss
+
+    def test_step(self, batch, batch_idx):
+        return self.validation_step(batch, batch_idx)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-5)
+        return optimizer
+
+
+def regular_training():
+    my_model = MLP_on_features()
+    my_model = my_model.to(device)
+    my_model.train()
+    dataset = CustomCLEVRImageDataset(DATA_SIZE)
+    TEST_SIZE = int(0.2 * DATA_SIZE)
+    train, val = random_split(dataset, [DATA_SIZE - TEST_SIZE, TEST_SIZE])
+    trainloader, val = DataLoader(train, batch_size=BATCH_SIZE), DataLoader(val, batch_size=BATCH_SIZE)
+    optimizer = torch.optim.Adam(my_model.parameters(), lr=1e-4)
+
+    for epoch in range(100):  # loop over the dataset multiple times
+
+        accuracies = [0 for i in range(amount_of_relations_types)]
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            x, y = data
+            y = y.to(torch.float32)
+            # x = x.view(x.size(0), -1)
+            y_hat = my_model(x)
+            # loss = F.mse_loss(y_hat, y.to(torch.float32))
+            # todo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # todo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            total_loss = 0
+            for k in range(amount_of_relations_types):
+                total_loss += losses[k](y_hat[:, k, :], y[:, k].long())
+                accuracies[k] += int(np.argmax(y_hat[:, k, :].detach().numpy()) == y[:, k].long()) / BATCH_SIZE
+            print(total_loss.item(), "      ", np.array(accuracies) / (i + 1))
+
+            total_loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += total_loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+    print('Finished Training')
+
+
+def run_exp():
+    dataset = CustomCLEVRImageDataset(DATA_SIZE)
+    test_size = int(DATA_SIZE * 0.2)
+    train, val = random_split(dataset, [DATA_SIZE - test_size, test_size])
+    my_model = MLP_on_features().to(device)
+    my_model.feat_extract.to(device)
+    trainer = pl.Trainer(gpus=GPUS, max_epochs=50, progress_bar_refresh_rate=20)
+    trainer.fit(my_model, DataLoader(train, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS),
+                DataLoader(val, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS))
+
+
+if __name__ == '__main__':
+    # regular_training()
+    # exit()
+    dataset = CustomCLEVRImageDataset(DATA_SIZE)
+    test_size = int(DATA_SIZE * 0.2)
+    train, val = random_split(dataset, [DATA_SIZE - test_size, test_size])
+    my_model = MLP_on_features().to(device)
+    my_model.feat_extract.to(device)
+    trainer = pl.Trainer(gpus=GPUS, max_epochs=50, progress_bar_refresh_rate=20)
+    trainer.fit(my_model, DataLoader(train, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS), DataLoader(val, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS))
